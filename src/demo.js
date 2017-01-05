c79f559e3644cef0e83d4581e5141a3f45a0bdc5
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-05 14:22:46 -0500

    Monkey patch requirejs so that defaults can be set (overridden in production)

--- src/demo.js
+++ src/demo.js
@@ -1,11 +1,8 @@
-/* Provide paths for any additional dependencies: */
 /*
- requirejs.config({
-  "paths": {
-    "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/4.4.0/d3.min"
-  }
-});
-*/
+ requirejs.default_paths({
+   "d3": "https://cdnjs.cloudflare.com/ajax/libs/d3/4.4.0/d3.min"
+ });
+ */
 define([/* List dependencies here as strings. */],
     function (/* Dependencies are instantiated here. */) {
       return function (query) {
